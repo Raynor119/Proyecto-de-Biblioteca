56ac9bcdb011beac18228d106bce6b0998f1781e
raynor119 <deivicamiloflorezprieto@gmail.com>
2020-06-05 22:38:45 -0500

    alpha0.4

---
+++ app/src/main/java/com/pixels/bdpbiblio/ItemDetailFragment1.java
@@ -0,0 +1,72 @@
+package com.pixels.bdpbiblio;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.design.widget.CollapsingToolbarLayout;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.pixels.bdpbiblio.dummy.DummyContent;
+import android.widget.*;
+
+/**
+ * A fragment representing a single Item detail screen.
+ * This fragment is either contained in a {@link ItemListActivity}
+ * in two-pane mode (on tablets) or a {@link ItemDetailActivity}
+ * on handsets.
+ */
+public class ItemDetailFragment1 extends Fragment {
+    /**
+     * The fragment argument representing the item ID that this fragment
+     * represents.
+     */
+    public static final String ARG_ITEM_ID = "item_id";
+
+    /**
+     * The dummy content this fragment is presenting.
+     */
+    private DummyContent.DummyItem mItem;
+
+    /**
+     * Mandatory empty constructor for the fragment manager to instantiate the
+     * fragment (e.g. upon screen orientation changes).
+     */
+    public ItemDetailFragment1() {
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        if (getArguments().containsKey(ARG_ITEM_ID)) {
+            // Load the dummy content specified by the fragment
+            // arguments. In a real-world scenario, use a Loader
+            // to load content from a content provider.
+            mItem = DummyContent.ITEM_MAP.get(getArguments().getString(ARG_ITEM_ID));
+
+            Activity activity = this.getActivity();
+            CollapsingToolbarLayout appBarLayout = (CollapsingToolbarLayout) activity.findViewById(R.id.toolbar_layout);
+            if (appBarLayout != null) {
+				Toast.makeText(getActivity(), mItem.content+"", Toast.LENGTH_LONG).show();
+                appBarLayout.setTitle(mItem.content);
+            }
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.item_detail1, container, false);
+
+        // Show the dummy content as text in a TextView.
+        if (mItem != null) {
+			Toast.makeText(getActivity(), mItem.details+"devoluciones", Toast.LENGTH_LONG).show();
+            ((TextView) rootView.findViewById(R.id.item_detail1)).setText(mItem.details);
+        }
+
+        return rootView;
+    }
+}
